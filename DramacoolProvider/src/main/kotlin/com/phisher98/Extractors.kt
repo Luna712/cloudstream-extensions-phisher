4e19c9df7da25a67ec2cbe6ffe81c1b6acce509b
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-01-11 15:40:51 -0700

    Add

--- DramacoolProvider/src/main/kotlin/com/phisher98/Extractors.kt
+++ DramacoolProvider/src/main/kotlin/com/phisher98/Extractors.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.argamap
+import com.lagradost.cloudstream3.extractors.Dwish
 import com.lagradost.cloudstream3.extractors.MixDrop
 import com.lagradost.cloudstream3.extractors.VidhideExtractor
 import com.lagradost.cloudstream3.extractors.helper.GogoHelper
@@ -12,12 +13,15 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.httpsify
 import com.lagradost.cloudstream3.utils.loadExtractor
 
-
 class dlions : VidhideExtractor() {
     override var name = "Dlions"
     override var mainUrl = "https://dlions.pro"
 }
 
-class MixDropSi : MixDrop(){
+class MixDropSi : MixDrop() {
     override var mainUrl = "https://mixdrop.si"
+}
+
+class PeytonePre : Dwish() {
+    override var mainUrl = "https://peytonepre.com"
 }
