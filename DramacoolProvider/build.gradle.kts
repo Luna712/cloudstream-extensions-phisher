4b31f44c71d0ff8f8e4bba599ed5d82161a13eda
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-01-11 16:39:38 -0700

    Bump

--- DramacoolProvider/build.gradle.kts
+++ DramacoolProvider/build.gradle.kts
@@ -1,11 +1,11 @@
 // use an integer for version numbers
-version = 13
+version = 14
 
 cloudstream {
     language = "en"
     // All of these properties are optional, you can safely remove them
 
-    description = "Kdrama"
+    description = "Asian Dramas"
     authors = listOf("Phisher98")
 
     /**
