b8cef98aac1685e405cdfe1c73bd452d13ff6fc9
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-01-12 17:23:31 -0700

    Try

--- DramacoolProvider/src/main/kotlin/com/phisher98/Extractors.kt
+++ DramacoolProvider/src/main/kotlin/com/phisher98/Extractors.kt
@@ -12,7 +12,15 @@ import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.httpsify
 import com.lagradost.cloudstream3.utils.loadExtractor
+
+import android.widget.Toast
 import com.lagradost.cloudstream3.CommonActivity.showToast
+
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.USER_AGENT
+import com.lagradost.cloudstream3.utils.M3u8Helper
+import com.lagradost.cloudstream3.utils.getAndUnpack
+import com.lagradost.cloudstream3.utils.getPacked
 
 class dlions : VidhideExtractor() {
     override var name = "Dlions"
@@ -28,12 +36,53 @@ class DramacoolExtractor : StreamWishExtractor() {
     override var mainUrl = "https://dramacool.men"
 }
 
-class Vkspeed : StreamWishExtractor() {
-    override var mainUrl = "https://asianload.org"
+class Vkspeed : ExtractorApi() {
+    override val name = "AsianLoad"
+    override val mainUrl = "https://asianload.org"
+    override val requiresReferer = true
 
     override fun getExtractorUrl(id: String): String {
-        showToast("id: $id")
+        showToast("id: $id", Toast.LENGTH_LONG)
         return "$mainUrl/view?id=$id"
+    }
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        showToast("url: $url", Toast.LENGTH_LONG)
+        val headers = mapOf(
+            "Accept" to "*/*",
+            "Connection" to "keep-alive",
+            "Sec-Fetch-Dest" to "empty",
+            "Sec-Fetch-Mode" to "cors",
+            "Sec-Fetch-Site" to "cross-site",
+            "Origin" to "$mainUrl/",
+            "User-Agent" to USER_AGENT
+        )
+        val response = app.get(url, referer = referer)
+        val script = if (!getPacked(response.text).isNullOrEmpty()) {
+            getAndUnpack(response.text)
+        } else if (!response.document.select("script").firstOrNull {
+                it.html().contains("jwplayer(\"vplayer\").setup(")
+            }?.html().isNullOrEmpty()
+        ) {
+            response.document.select("script").firstOrNull {
+                it.html().contains("jwplayer(\"vplayer\").setup(")
+            }?.html()
+        } else {
+            response.document.selectFirst("script:containsData(sources:)")?.data()
+        }
+        val m3u8 =
+            Regex("file:\\s*\"(.*?m3u8.*?)\"").find(script ?: return)?.groupValues?.getOrNull(1)
+        M3u8Helper.generateM3u8(
+            name,
+            m3u8 ?: return,
+            mainUrl,
+            headers = headers
+        ).forEach(callback)
     }
 }
 
