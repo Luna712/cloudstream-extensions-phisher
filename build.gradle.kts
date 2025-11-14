d4f810157f958a42a03444aaf316be49d90c5655
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-14 13:34:26 -0700

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.1")
-        classpath("com.github.Luna712:gradle:-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
     }
 }
