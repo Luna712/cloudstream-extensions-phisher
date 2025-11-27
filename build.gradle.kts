d0b87d5cd0048d1a38d0382e513e7a3af91308de
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-26 18:05:20 -0700

    Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,7 +85,7 @@ subprojects {
     dependencies {
         val implementation by configurations
         val cloudstream by configurations
-        cloudstream("com.lagradost:cloudstream3:pre-release")
+        cloudstream("com.lagradost:cloudstream3:v4.6.0")
 
         // Other dependencies
         implementation(kotlin("stdlib"))
